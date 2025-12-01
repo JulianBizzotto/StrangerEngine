5c3b3b90724ba8dba5b39f879537dc835be3e07f
Julian Bizzotto <thesurvivorselpelado@gmail.com>
2025-11-30 23:20:19 -0300

    Window added succesfully

--- src/main.cpp
+++ src/main.cpp
@@ -1,92 +1,126 @@
 #include <iostream>
+#include <stdint.h> // Para usar tipos de tamaño fijo (uint8_t, uint32_t)
+
+// ##################################################################
+//                          Platform Globals
+// ##################################################################
+static bool running = true;
+
+
+
+// ##################################################################
+//                          Platform Functions
+// ##################################################################
+
+bool platform_create_window(int width, int height, char* title); 
+void platform_update_window();
+// ##################################################################
+//                          Windows Platform
+// ##################################################################
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
 #include <windows.h>
 
-// Prototipo del procedimiento de ventana (el cerebro que maneja los eventos)
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+// ##################################################################
+//                          Windows Globals
+// ##################################################################
+static HWND window;
 
-int main() {
+// ##################################################################
+//                         Platform Implementations 
+// ##################################################################
+LRESULT CALLBACK windows_window_callback(HWND window, UINT msg,
+                                        WPARAM wParam, LPARAM lParam)
+{
+    LRESULT result = 0;
+
+    switch (msg)
+    {
+        case WM_CLOSE:
+        
+        {
+            running = false;
+            break;
+        }
+        
+        default:
+        {
+            // Let windows handle the default input for now
+            result = DefWindowProcA(window, msg, wParam, lParam);
+        }
+    }
+
+    return result;
+}
+
+bool platform_create_window(int width, int height, char* title) 
+{
+    HINSTANCE instance = GetModuleHandleA(0);
+
+    WNDCLASSA wc = {};
+    wc.hInstance = instance;
+    wc.hIcon = LoadIcon(instance, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW); // This means we decide the look of the cursor(arrow)
+    wc.lpszClassName = title; // This is NOT the title, just a unique identifier
+    wc.lpfnWndProc = windows_window_callback; // Callback for Input into the Window
+
+    if(!RegisterClassA(&wc)) return false; 
+
+
+
+    // WS_CAPTION | WS_SYSMENU | WS_THICKFRAME | WS_MINIMIZEBOX | WS_MAXIMIZEBOX
+    int dwStyle = WS_OVERLAPPEDWINDOW;
+
+    window = CreateWindowExA(0, title,  //This reference lpszClassName from wc
+        title,  //This is the actual Title
+        dwStyle,
+        100,
+        100,
+        width,
+        height,
+        NULL, // parent
+        NULL, //menu
+        instance,
+        NULL //lpParam
+        );
+    
+    if(window == NULL) return false;
+
+    ShowWindow(window, SW_SHOW);
+
+    return true;
+
+}
+
+void platform_update_window(){
+    MSG msg;
+
+    while(PeekMessageA(&msg, window, 0, 0, PM_REMOVE)){
+        TranslateMessage(&msg);
+        DispatchMessageA(&msg); // Calls the callback specified when creating  the window
+    }
+}
+
+#endif
+
+
+int main() { 
     std::cout << "Inicializando strangerEngine..." << std::endl;
 
-    // 1. Obtener el manejador de la instancia actual (Necesario porque usamos main estándar)
-    HINSTANCE hInstance = GetModuleHandle(NULL);
+    int width = 1200;
+    int height = 720;
 
-    // 2. Definir la clase de ventana
-    const char* CLASS_NAME = "StrangerEngineWindowClass";
-    
-    WNDCLASS wc = { };
-    wc.lpfnWndProc = WindowProc;   // Puntero a la función que maneja los mensajes
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW); // Cursor normal
+    char* title = "strangerEngine v0.1";
 
-    // 3. Registrar la clase
-    if (!RegisterClass(&wc)) {
-        std::cout << "Fallo al registrar la ventana." << std::endl;
-        return -1;
-    }
-
-    // 4. Crear la ventana
-    HWND hwnd = CreateWindowEx(
-        0,                              // Estilos opcionales
-        CLASS_NAME,                     // Nombre de la clase
-        "Stranger Engine v0.1",         // Título de la barra
-        WS_OVERLAPPEDWINDOW,            // Ventana con bordes y botones estándar
-        CW_USEDEFAULT, CW_USEDEFAULT,   // Posición X, Y
-        1280, 720,                      // Resolución inicial
-        NULL,                           // Ventana padre
-        NULL,                           // Menú
-        hInstance,                      // Instancia
-        NULL                            // Puntero adicional
-    );
-
-    if (hwnd == NULL) {
-        std::cout << "Fallo al crear la ventana." << std::endl;
-        return -1;
-    }
-
-    // 5. Mostrar la ventana
-    ShowWindow(hwnd, SW_SHOW);
+    platform_create_window(width, height, title);
 
     std::cout << "Motor corriendo. Cierra la ventana para terminar." << std::endl;
 
-    // 6. El Bucle Principal (Game Loop)
-    MSG msg = { };
-    while (GetMessage(&msg, NULL, 0, 0) > 0) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    while(running){
+        platform_update_window();
     }
 
     std::cout << "Apagando strangerEngine." << std::endl;
     return 0;
-}
-
-// Implementación del manejo de eventos de Windows
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    switch (uMsg) {
-    case WM_CLOSE:
-        // Mensaje cuando aprietas la X de la ventana
-        DestroyWindow(hwnd);
-        return 0;
-
-    case WM_DESTROY:
-        // Mensaje cuando la ventana ya se destruyó -> Salimos del loop
-        PostQuitMessage(0);
-        return 0;
-
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
-
-            // Rellenar la pantalla de un color solido (Gris oscuro)
-            HBRUSH brush = CreateSolidBrush(RGB(40, 40, 40));
-            FillRect(hdc, &ps.rcPaint, brush);
-            DeleteObject(brush); // Importante: borrar objetos GDI para no fugar memoria
-
-            EndPaint(hwnd, &ps);
-        }
-        return 0;
-    }
-    
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
